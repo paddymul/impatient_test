cc1d157f5279e311808d9cc3109a38af9d864eab
Paddy Mullen <paddy@chartwidget.com>
2010-12-10 12:29:42 -0500

    cleaned up

--- impatient_test/tests/collection_test.py
+++ impatient_test/tests/collection_test.py
@@ -9,20 +9,12 @@ class CollectionTest(unittest.TestCase):
     
     def test_stdout_collection(self):
         tr = collect_test("output_fixture.OutputTestKlass.test_stdout")
-        #print stdout_string
-        #print tr.stdout
-        #pdb.set_trace()
         self.assertTrue(stdout_string in tr.stdout)
 
     def test_stderr_collection(self):
         tr = collect_test("output_fixture.OutputTestKlass.test_stderr")
-        #print stdout_string
-        #print tr.stdout
-        #pdb.set_trace()
         self.assertTrue(stderr_string in tr.stderr)
 
-
-        #print tr.stdout
 
         
 if __name__ == '__main__':
